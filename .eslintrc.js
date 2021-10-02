97b5567dec1f34d10126e3cbc4ba5817b7eae842
Artem Shuvaev <ashuvaev@ozon.ru>
2021-10-02 12:59:02 +0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    "plugin:vue/base"
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+}
